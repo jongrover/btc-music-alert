ef67348b1afc2a0d0d8e261bf76f57bb8c6cdf96
Jon <64977099+hipmeltypikachu@users.noreply.github.com>
2020-05-07 23:22:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,97 +1,76 @@
 $(function () {
 
-var api = 'https://www.bitstamp.net/api/ticker/',
-    checkInterval = 1000; // milliseconds (max requests 800 requests per minute)
+  var api = 'https://www.bitstamp.net/api/ticker/',
+      checkInterval = 200, // in milliseconds (max requests 800 requests per minute)
+      livePrice = 0.00,
+      $livePrice = $('#live-price'),
+      $audio1 = $('#alert-1').find('audio'),
+      $audio2 = $('#alert-2').find('audio'),
+      $audio3 = $('#alert-3').find('audio'),
+      trigger1 = '', trigger2 = '', trigger3 = '',
+      opt1 = '', opt2 = '', opt3 = '',
+      active = false;
 
-  function checkPriceAlert1() {
-    var $parent = $('#alert-1'),
-        $currPrice = $('#current-price'),
-        $audio = $parent.find('audio'),
-        option = $parent.data('opt'),
-        trigger = parseFloat($parent.data('trigger'));
-    $.ajax({
-      url: api,
-      type: 'post',
-      success: function (data){
-       var currPrice = parseFloat(data.last);
-       $currPrice.text(currPrice);
-       if (option == 'greater') {
-         if (currPrice >= trigger) {
-           console.log('Price greater than your alert!');
-           $audio[0].play();
-         }
-       } else {
-         if (currPrice <= trigger) {
-           console.log('Price less than your alert!');
-           $audio[0].play();
-         }
-       }
-      },
-      complete:function(data){
-       setTimeout(checkPriceAlert1, checkInterval);
+  function checkAlert() {
+    if (trigger1 !== '' && opt1 !== '') {
+      if (opt1 === 'greater') {
+        if (livePrice >= trigger1) {
+          $audio1[0].play();
+          console.log('alert 1 triggered: price greater than $'+trigger1);
+        }
+      } else {
+        if (livePrice <= trigger1) {
+          $audio1[0].play();
+          console.log('alert 1 triggered: price less than $'+trigger1);
+        }
       }
-     });
+    }
+    if (trigger2 !== '' && opt2 !== '') {
+      if (opt2 === 'greater') {
+        if (livePrice >= trigger2) {
+          $audio2[0].play();
+          console.log('alert 2 triggered: price greater than $'+trigger2);
+        }
+      } else {
+        if (livePrice <= trigger2) {
+          $audio2[0].play();
+          console.log('alert 2 triggered: price less than $'+trigger2);
+        }
+      }
+    }
+    if (trigger3 !== '' && opt3 !== '') {
+      if (opt3 === 'greater') {
+        if (livePrice >= trigger3) {
+          $audio3[0].play();
+          console.log('alert 3 triggered: price greater than $'+trigger3);
+        }
+      } else {
+        if (livePrice <= trigger3) {
+          $audio3[0].play();
+          console.log('alert 3 triggered: price less than $'+trigger3);
+        }
+      }
+    }
   }
 
-  function checkPriceAlert2() {
-    var $parent = $('#alert-2'),
-        $currPrice = $('#current-price'),
-        $audio = $parent.find('audio'),
-        option = $parent.data('opt'),
-        trigger = parseFloat($parent.data('trigger'));
+  function upDatePrice() {
     $.ajax({
       url: api,
       type: 'post',
       success: function (data){
-       var currPrice = parseFloat(data.last);
-       $currPrice.text(currPrice);
-       if (option == 'greater') {
-         if (currPrice >= trigger) {
-           console.log('Price greater than your alert!');
-           $audio[0].play();
-         }
-       } else {
-         if (currPrice <= trigger) {
-           console.log('Price less than your alert!');
-           $audio[0].play();
-         }
+       livePrice = parseFloat(data.last);
+       $livePrice.text(livePrice);
+       //console.log('price updated to: $'+livePrice);
+       if (active) {
+         checkAlert();
        }
       },
       complete:function(data){
-       setTimeout(checkPriceAlert2, checkInterval);
+       setTimeout(upDatePrice, checkInterval);
       }
      });
   }
-    
-  function checkPriceAlert3() {
-    var $parent = $('#alert-3'),
-        $currPrice = $('#current-price'),
-        $audio = $parent.find('audio'),
-        option = $parent.data('opt'),
-        trigger = parseFloat($parent.data('trigger'));
-    $.ajax({
-      url: api,
-      type: 'post',
-      success: function (data){
-       var currPrice = parseFloat(data.last);
-       $currPrice.text(currPrice);
-       if (option == 'greater') {
-         if (currPrice >= trigger) {
-           console.log('Price greater than your alert!');
-           $audio[0].play();
-         }
-       } else {
-         if (currPrice <= trigger) {
-           console.log('Price less than your alert!');
-           $audio[0].play();
-         }
-       }
-      },
-      complete:function(data){
-       setTimeout(checkPriceAlert3, checkInterval);
-      }
-     });
-  }
+  upDatePrice();
 
   $('form').submit(function (event) {
     event.preventDefault();
@@ -105,17 +84,23 @@ var api = 'https://www.bitstamp.net/api/ticker/',
         $alertPrice = $parent.find('.alert-price');
     $audio[0].pause();
     $audio[0].currentTime = 0;
-    $parent.data('trigger', $price.val());
-    $parent.data('opt', $opt.val());
-    $optChoice.text($parent.data('opt')+' than');
-    $alertPrice.text($parent.data('trigger'));
     if (id == 'alert-1') {
-      checkPriceAlert1();
+      trigger1 = parseFloat($price.val());
+      opt1 = $opt.val();
+      $optChoice.text(opt1+' than');
+      $alertPrice.text(trigger1);
     } else if (id == 'alert-2') {
-      checkPriceAlert2();
-    } else {
-      checkPriceAlert3();
+      trigger2 = parseFloat($price.val());
+      opt2 = $opt.val();
+      $optChoice.text(opt2+' than');
+      $alertPrice.text(trigger2);
+    } else if (id == 'alert-3') {
+      trigger3 = parseFloat($price.val());
+      opt3 = $opt.val();
+      $optChoice.text(opt3+' than');
+      $alertPrice.text(trigger3);
     }
+    active = true;
   });
 
   $('.clear').click(function (event) {
@@ -130,8 +115,19 @@ var api = 'https://www.bitstamp.net/api/ticker/',
     $audio[0].pause();
     $audio[0].currentTime = 0;
     $price.val('');
-    $parent.data('trigger', '');
-    $parent.data('opt', '');
+    if (id === 'alert-1') {
+      trigger1 = '';
+      opt1 = '';
+      console.log('clear alert 1!');
+    } else if (id === 'alert-2') {
+      trigger2 = '';
+      opt2 = '';
+      console.log('clear alert 2!');
+    } else if (id === 'alert-3') {
+      trigger3 = '';
+      opt3 = '';
+      console.log('clear alert 3!');
+    }
     $optChoice.text('...');
     $alertPrice.text('...');
   });
